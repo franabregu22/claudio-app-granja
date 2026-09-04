877a5c29d94510613a10257eceac76a6d644cb22
Francisco Abregú <abregufrancisco99@gmail.com>
2026-09-04 12:50:58 -0300

    Try: YYYY-MM-DD date format for settlement

--- netlify/functions/find-statement-endpoint.ts
+++ netlify/functions/find-statement-endpoint.ts
@@ -18,8 +18,8 @@ const handler: Handler = async (event) => {
 
     const now = new Date();
     const ninetyDaysAgo = new Date(now.getTime() - 90 * 24 * 60 * 60 * 1000);
-    const beginDate = ninetyDaysAgo.toISOString();
-    const endDate = now.toISOString();
+    const beginDate = ninetyDaysAgo.toISOString().split("T")[0];
+    const endDate = now.toISOString().split("T")[0];
 
     const endpoints = [
       { url: "/v1/account/settlement_report", name: "settlement_report" },
